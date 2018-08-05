7fc7fc01d69f46f3da35b773b74b9c868e9fd0cf
LLLLLLLL <33690471+YOhonour@users.noreply.github.com>
2018-08-06 01:49:31 +0800

    Update Books.java

--- src/Books.java
+++ src/Books.java
@@ -6,11 +6,11 @@ public class Books {
 
     private int count = 1;//书的数量
     private boolean flag = false;//判断是否有进程在操作的标记变量
-    private boolean isFull = false;//库存是否
+    private boolean isFull = false;//库存是否满的标记变量
     private int runN = 0;
 
-    Lock lock = new ReentrantLock();
-    Condition set_con = lock.newCondition();
+    Lock lock = new ReentrantLock();//实例化锁对象
+    Condition set_con = lock.newCondition();//获取两个线程的监视器对象
     Condition out_con = lock.newCondition();
 
     public boolean set(){
@@ -24,7 +24,7 @@ public class Books {
             runN++;
              System.out.println("还书线程"+Thread.currentThread().getName()+"图书剩余："+count+"本");
             out_con.signal();
-            if(runN<5)
+            if(runN<5)//还书线程运行低于五次时，没还一次等待一次防止线程提前结束
                 set_con.await();
         }catch (InterruptedException e){}
         finally {
@@ -32,7 +32,7 @@ public class Books {
             while (runN > 10)
             {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1000);//当还书线程运行10次后尝试休眠以增加借书完全的几率
                 } catch (InterruptedException e) {
                 }
             }
